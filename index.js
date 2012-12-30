8e62dbbb8346b16b9dc565f375844edf4fc72b0f
James Halliday <mail@substack.net>
2012-12-29 18:09:53 -0800

    path.sep || "/"

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = function (pdir_, dir_) {
-    var pdir = path.resolve(path.normalize(pdir_)) + path.sep;
+    var pdir = path.resolve(path.normalize(pdir_)) + (path.sep || '/');
     var dir = path.resolve(pdir, path.normalize(dir_));
     return dir.slice(0, pdir.length) === pdir;
 };
