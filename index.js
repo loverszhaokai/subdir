060b2ba38defa2255354fb35a434a4865dbb3883
James Halliday <mail@substack.net>
2013-07-10 05:48:46 -0700

    fix for the failing test

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var path = require('path');
 module.exports = function (pdir_, dir_) {
     var pdir = path.resolve(path.normalize(pdir_)) + (path.sep || '/');
     var dir = path.resolve(pdir, path.normalize(dir_));
+    if (pdir === '//') pdir = '/';
     return dir.slice(0, pdir.length) === pdir;
 };
